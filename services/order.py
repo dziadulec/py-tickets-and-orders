dfe9f9823c550760e3e7f64cf6d1ac9d1c704a9f
Marek Drządzewski <dzadzumaster@gmail.com>
2025-12-17 18:01:03 +0100

    Add create_order and get_orders functions

---
+++ services/order.py
@@ -0,0 +1,46 @@
+from typing import List
+from datetime import datetime
+from db.models import Ticket, User, Order, MovieSession
+from django.db import transaction
+
+
+@transaction.atomic
+def create_order(tickets: List[dict],
+                 username: str,
+                 date: datetime = None
+                 ) -> List[Ticket]:
+    user = User.objects.get(username=username)
+    order = Order.objects.create(user=user)
+
+    if date:
+        order.created_at = date
+        order.save()
+    created_tickets = []
+
+    for ticket_dic in tickets:
+
+        try:
+            row = ticket_dic.get("row")
+            seat = ticket_dic.get("seat")
+            movie_session_id = ticket_dic.get("movie_session")
+            movie_session = MovieSession.objects.get(id=movie_session_id)
+
+        except KeyError as e:
+            print(e)
+
+        ticket = Ticket.objects.create(
+            movie_session=movie_session,
+            order=order,
+            row=row,
+            seat=seat
+        )
+        created_tickets.append(ticket)
+
+    return ticket
+
+
+def get_orders(username: str = None):
+    orders = Order.objects.all()
+    if username:
+        orders = orders.filter(user__username=username)
+    return orders
