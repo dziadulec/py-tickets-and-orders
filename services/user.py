8f35520544501cd68ae36532d56baa54cab6ca80
Marek Drządzewski <dzadzumaster@gmail.com>
2025-12-17 12:39:14 +0100

    Add a file and the create_user function inside it

---
+++ services/user.py
@@ -0,0 +1,23 @@
+from db.models import User
+
+
+def create_user(
+        username: str,
+        password: str,
+        email: str = None,
+        first_name: str = None,
+        last_name: str = None,
+) -> User:
+    user = User.objects.create_user(username=username, password=password)
+
+    if email:
+        user.email = email
+
+    if first_name:
+        user.first_name = first_name
+
+    if last_name:
+        user.last_name = last_name
+
+    user.save()
+    return user
