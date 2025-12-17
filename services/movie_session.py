c2403aeff4d1b4ca34681da3384ad6951280b778
Marek Drządzewski <dzadzumaster@gmail.com>
2025-12-17 17:58:13 +0100

    Add get_taken_seats function

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,7 +1,7 @@
 from django.db.models import QuerySet
 
-from db.models import MovieSession
-
+from db.models import MovieSession, Ticket
+from typing import Any, List
 
 def create_movie_session(
     movie_show_time: str, movie_id: int, cinema_hall_id: int
@@ -42,3 +42,10 @@ def update_movie_session(
 
 def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
+
+
+def get_taken_seats(movie_session_id: int) -> List[Ticket]:
+    tickets = Ticket.objects.select_related("movie_session").filter(
+        movie_session_id=movie_session_id)
+
+    return [{"row": ticket.row, "seat": ticket.seat} for ticket in tickets]
